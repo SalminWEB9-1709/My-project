0f6d913dfa197c9f8facb090c5fe2d9dc19f6877
SalminWEB9-1709 <155303239+SalminWEB9-1709@users.noreply.github.com>
2024-03-19 10:10:16 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1 className='text-6xl bg-rose-700 text-center'>Bangladesh Birth Rate 1950-2024</h1>
+    </>
+  )
+}
+
+export default App
